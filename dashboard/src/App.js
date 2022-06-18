e0db4dc3e5581bfcd877ca5c8a7ab516c04a9b96
KareemElozeiri <K.elozeiri@gmail.com>
2022-06-19 01:20:26 +0200

    few adds

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="content">
+        <Switch>
+          <Route exact path="/">
+            <div>
+              <h1>index</h1>
+            </div>
+          </Route>
+          <Route exact path="/profile/:profile_id">
+            <h1>profile</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
